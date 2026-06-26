8c292afc45d6f28573c543b14a458fc8baab053f
Riz Michael Sim <sim.rizmichael@gmail.com>
2026-06-26 17:34:24 +0800

    used migrate:fresh

--- backend/scripts/render-start.sh
+++ backend/scripts/render-start.sh
@@ -3,8 +3,8 @@
 set -eu
 
 php artisan optimize:clear --ansi
-php artisan migrate --force --ansi
-php artisan db:seed --force --ansi
+php artisan migrate:fresh --seed --ansi
+
 
 if php artisan storage:link --ansi; then
   echo "Storage link is ready."
