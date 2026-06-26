4dcc3ffe7e7215e582293f94f5c5c6800434f872
Riz Michael Sim <sim.rizmichael@gmail.com>
2026-06-26 17:55:59 +0800

    patched controller to no-cache

--- backend/app/Modules/Appointments/Controllers/ListAppointmentTypesController.php
+++ backend/app/Modules/Appointments/Controllers/ListAppointmentTypesController.php
@@ -15,6 +15,8 @@ class ListAppointmentTypesController extends Controller
 
         return response()->json([
             'data' => $appointmentTypes,
-        ]);
+        ])->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+            ->header('Pragma', 'no-cache')
+            ->header('Expires', '0');
     }
 }
